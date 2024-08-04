1ab971e28c1d653630871a427191d9b89f623ad8
bagusindrayana <bagusindrayanaindo@gmail.com>
2024-08-04 13:04:04 +0800

    rate limit

--- index.js
+++ index.js
@@ -22,8 +22,8 @@ const options = [
 app.use(options);
 
 const limiter = rateLimit({
-	windowMs: 15 * 60 * 1000, // 15 minutes
-	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	windowMs: 60 * 1000, // 1 minutes
+	limit: 100, // Limit each IP to 100 requests per `window`.
 	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
 	// store: ... , // Redis, Memcached, etc. See below.
