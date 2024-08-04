78a07a8208f1aebce2dec80869b403c97ab403cd
bagusindrayana <bagusindrayanaindo@gmail.com>
2024-08-04 14:58:42 +0800

    merge master

--- index.js
+++ index.js
@@ -42,25 +42,6 @@ async function generateContent (model,prompt)  {
 
 const app = express();
 
-// const options = [
-//     cors({
-//         origin: ["roastgithub.netlify.app","roastgithub.vercel.app","https://roastgithub.netlify.app","https://roastgithub.vercel.app"],
-//     })
-// ];
-
-var allowlist = ["roastgithub.netlify.app","roastgithub.vercel.app","https://roastgithub.netlify.app","https://roastgithub.vercel.app","http://roastgithub.netlify.app","http://roastgithub.vercel.app"]
-var corsOptionsDelegate = function (req, callback) {
-  var corsOptions;
- 
-  if (allowlist.indexOf(req.header('Origin')) !== -1) {
-    corsOptions = { origin: true }
-  } else {
-    corsOptions = { origin: false }
-    console.log(`Not Found : ${req.header('Origin')}`);
-  }
-  callback(null, corsOptions)
-}
-app.use(cors(corsOptionsDelegate));
 
 const limiter = rateLimit({
     windowMs: 2 * 60 * 1000, // 15 minutes
