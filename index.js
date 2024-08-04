71ae474784d2580f46dd04ecb4cf1b8aec7fdc29
bagusindrayana <bagusindrayanaindo@gmail.com>
2024-08-04 16:35:20 +0800

    kurangi limit

--- index.js
+++ index.js
@@ -62,8 +62,8 @@ var corsOptionsDelegate = function (req, callback) {
 app.use(cors(corsOptionsDelegate));
 
 const limiter = rateLimit({
-    windowMs: 1 * 60 * 1000, // 15 minutes
-    limit: 30, // Limit each IP to 100 requests per `window`.
+    windowMs: 1 * 60 * 1000, // minutes
+    limit: 10, // Limit each IP per `window`.
     standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
     // store: ... , // Redis, Memcached, etc. See below.
